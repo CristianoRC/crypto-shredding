c55d612929c0c92a38fb678ab0b6444f87cb2a46
Cristiano Cunha <contato@cristianoprogramador.com>
2026-07-19 09:51:38 -0300

    Remove projeto antigo

--- CryptoShredding/CryptoShredding.Core/Entities/User.cs
+++
@@ -1,4 +0,0 @@
-namespace CryptoShredding.Core.Entities;
-
-//Poderia usar Value Objects, mas para esse exemplo só deixaria mais complexo...
-public record User(Guid Id, string Name, string Document, string Address);
--- CryptoShredding/CryptoShredding.Core/Services/CryptoService/EncryptionKey.cs
+++
@@ -1,7 +0,0 @@
-namespace CryptoShredding.Core.Services.CryptoService;
-
-public record EncryptionKey
-{
-    public required byte[] Key { get; init; }
-    public required byte[] InitializationVector { get; init; }
-}
